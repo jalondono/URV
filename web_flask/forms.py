91ff3bba6a54ae4728349d7cdc4b6f0b53905908
jalondono <juanlondono151776@hotmail.com>
2020-02-02 19:45:18 -0500

    * index.html - Change the data input, select input instead text input * main.css - Added style to select input * forms.py - Added changed the input text on the form

--- web_flask/forms.py
+++ web_flask/forms.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-from wtforms import Form, StringField, TextField
+from wtforms import Form, StringField, TextField, SelectField
 
 """ Class to manage de forms"""
 
 
 class CommentForms(Form):
     """ create the attributes """
-    place = StringField('place')
+    place = SelectField(
+        'place',
+        choices=[('Amazonas', 'Amazonas'), ('Antioquia', 'Antioquia'), ('Arauca', 'Arauca'),
+                 ('Atlántico', 'Atlántico'), ('Bolívar', 'Bolívar'), ('Boyacá', 'Boyacá'),
+                 ('Caldas', 'Caldas'), ('Caquetá', 'Caquetá'), ('Casanare', 'Casanare'),
+                 ('Cauca', 'Cauca'), ('Cesar', 'Cesar'), ('Chocó', 'Chocó'),
+                 ('Córdoba', 'Córdoba'), ('Cundinamarca', 'Cundinamarca'), ('Guainía', 'Guainía'),
+                 ('Guaviare', 'Guaviare'), ('Huila', 'Huila'), ('La Guajira', 'La Guajira'),
+                 ('Magdalena', 'Magdalena'), ('Meta', 'Meta'), ('Nariño', 'Nariño'),
+                 ('Norte de Santander', 'Norte de Santander'), ('Putumayo', 'Putumayo'), ('Quindío', 'Quindío'),
+                 ('Tolima', 'Tolima'), ('Valle del Cauca', 'Valle del Cauca'), ('Vaupés', 'Vaupés'),
+                 ('Vichada', 'Vichada')]
+    )
+
+    """place = StringField('place')"""
     victims = StringField('victims')
